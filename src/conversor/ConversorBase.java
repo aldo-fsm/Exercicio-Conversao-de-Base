2d096a2b41a3c171fb7127b627811bb0eeb089ee
aldo <aldo@aldo-Inspiron-3443>
2016-08-17 16:06:13 -0300

    metodo converter

--- src/conversor/ConversorBase.java
+++ src/conversor/ConversorBase.java
@@ -5,11 +5,27 @@ public class ConversorBase {
 	private static final String digitos = "0123456789ABCDEF";
 
 	public String converter(String numero, int baseOrigem, int baseDestino) {
+
 		numero.toUpperCase();
 
-		return null;
-	} 
+		if (baseOrigem >= 2 && baseOrigem <= 16 && baseDestino >= 2 && baseDestino <= 16) {
 
+			if (baseDestino == baseOrigem) {
+				return numero;
+			} else if (baseOrigem == 10) {
+				return deDecimal(Integer.parseInt(numero), baseDestino);
+			} else if (baseDestino == 10) {
+				return Integer.toString(paraDecimal(numero, baseOrigem));
+			} else {
+				return deDecimal(paraDecimal(numero, baseDestino), baseOrigem);
+			}
+
+		}
+
+		return null;
+	}
+
+	// parte iterativa
 	private int paraDecimal(String numero, int base) {
 
 		int decimal = 0;
@@ -19,6 +35,13 @@ public class ConversorBase {
 			int valorDigito = digitos.indexOf(numero.charAt(i));
 			decimal += valorDigito * Math.pow(base, numeroDigitos - 1 - i);
 		}
+
 		return decimal;
+	}
+
+	// parte recursiva
+	private String deDecimal(int decimal, int baseDestino) {
+
+		return null;
 	}
 }
