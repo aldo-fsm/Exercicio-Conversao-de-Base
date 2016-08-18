2ab674d6f690718a9d0fcd8f29fe588c6e522726
guest <guest@localhost>
2016-08-18 03:42:59 -0300

    teste

--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -1,8 +1,9 @@
 package testes;
 
+import conversor.ConversorBase;
+
 public class Teste {
 	public static void main(String[] args) {
-
-		
+		System.out.println(ConversorBase.converter("44",5,9));
 	}
 }
