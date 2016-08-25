2a7db9155533c03a5ee8dbbfdf7d0e0442332f45
guest <guest@localhost>
2016-08-25 10:27:18 -0300

    ...

--- src/conversor/ConversorBaseBigDecimal.java
+++ src/conversor/ConversorBaseBigDecimal.java
@@ -2,14 +2,15 @@ package conversor;
 
 import java.math.BigDecimal;
 import java.security.InvalidParameterException;
+import java.util.Iterator;
 
 public class ConversorBaseBigDecimal {
 	private static final String digitos = "0123456789ABCDEF";
 
 	public static String converter(String numero, int baseOrigem, int baseDestino) {
-
+		
 		numero.toUpperCase();
-
+		
 		if (baseOrigem >= 2 && baseOrigem <= 16 && baseDestino >= 2 && baseDestino <= 16) {
 
 			if (verificarBase(numero, baseOrigem)) {
@@ -31,6 +32,58 @@ public class ConversorBaseBigDecimal {
 		} else {
 			throw new InvalidParameterException("Base invalida");
 		}
+	}
+	
+	public static String converterFracionario(String numero, int baseOrigem, int baseDestino) {
+
+		numero.toUpperCase();
+		String[] s;
+		s = numero.split(",");
+		
+		return converter(s[0], baseOrigem, baseDestino) + "." + deDecimalFracionario(paraDecimalFracionario(s[1], baseOrigem),baseDestino);
+	}
+	
+	
+	
+
+	private static BigDecimal paraDecimalFracionario(String numero, int baseOrigem) {
+		BigDecimal decimal = BigDecimal.ZERO;
+		long numeroDigitos = numero.length();
+		BigDecimal bigBase = BigDecimal.valueOf(baseOrigem);
+
+		for (int i = 0; i < numeroDigitos; i++) {
+			BigDecimal valorDigito = BigDecimal.valueOf(digitos.indexOf(numero.charAt(i)));
+			decimal = decimal.add(valorDigito.divide(bigBase.pow((int) (i+1))));
+		}
+
+		return decimal;
+	}
+
+	private static String deDecimalFracionario(BigDecimal decimal, int baseDestino) {
+	
+		if (decimal.compareTo(BigDecimal.valueOf(baseDestino)) < 0) {
+			return Character.toString(digitos.charAt(decimal.intValue()));
+		} else {
+			String s1 = decimal.toString();
+			String s = (decimal.multiply(BigDecimal.valueOf(baseDestino)).toString());
+			int numeroExcedido = 0;
+			if(s1.length()<s.length()){
+				String local = "";
+				for (int i = 0; i < s.length()-s1.length(); i++) {
+					local += s1.charAt(i);
+				}
+				numeroExcedido = Integer.parseInt(local);
+			}
+			
+			if(numeroExcedido == baseDestino-1){
+				return numeroExcedido + "";
+			}else{
+
+			}
+			
+		}
+		return null;
+		
 	}
 
 	private static boolean verificarBase(String numero, long base) {
--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -5,10 +5,9 @@ import conversor.ConversorBaseBigDecimal;
 
 public class Teste {
 	public static void main(String[] args) {
-		String numero = "51245680";
-		int bi = 10;
+		String numero = "ABCDEF0,12345";
+		int bi = 16;
 		int bf = 2;
-		System.out.println(ConversorBase.converter(numero,bi,bf));
-		System.out.println(ConversorBaseBigDecimal.converter(numero,bi,bf));
+		System.out.println(ConversorBaseBigDecimal.converterFracionario(numero, bi, bf));
 	}
 }
